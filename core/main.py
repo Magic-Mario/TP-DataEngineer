1d2594e327828b9771fda002466ca6f0114060f5
Magic-Mario <magic.mario2021@gmail.com>
2023-02-03 10:57:48 -0500

    API retrieve the data

--- core/main.py
+++ core/main.py
@@ -1,24 +1,30 @@
-#import Cleaning.cleaning
 #import polars as pl
+#from Cleaning.cleaning import *
 import numpy as np
 import pandas as pd
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, status
+from fastapi.responses import Response, JSONResponse
+from fastapi.exceptions import HTTPException
+from fastapi.params import Body
+from pydantic import BaseModel
 
 #* Cargar los archivos csv como dataframes
-amazon_df = pd.read_csv('../Datasets/amazon_prime_titles-score.csv')
-disney_plus_df = pd.read_csv('../Datasets/disney_plus_titles-score.csv')
-netflix_title_df = pd.read_csv('../Datasets/netflix_titles-score.csv')
-hulu_df = pd.read_csv('../Datasets/hulu_titles-score (2).csv')
+amazon_df = pd.read_csv('Datasets/amazon_prime_titles-score.csv')
+disney_plus_df = pd.read_csv('Datasets/disney_plus_titles-score.csv')
+netflix_title_df = pd.read_csv('Datasets/netflix_titles-score.csv')
+hulu_df = pd.read_csv('Datasets/hulu_titles-score (2).csv')
 
 
-df_dict = dict(amazon_df=amazon_df, disney_plus=disney_plus_df,
-               netflix=netflix_title_df, hulu=hulu_df)
+
+df_dict = {'amazon_df':amazon_df,
+            'disney_plus':disney_plus_df,
+            'netflix':netflix_title_df,
+            'hulu':hulu_df}
 
 app = FastAPI()
 
 
-@app.get('/{df}')
+@app.get('/{df_name}', status_code=status.HTTP_200_OK)
 async def get_dataframe(df_name: str):
     if df_name not in df_dict:
         return JSONResponse(content={"error": "DataFrame not found"})
